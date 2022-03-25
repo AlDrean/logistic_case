52d8fc1e94d8d009ad688de48ccc30fb9b1862d5
AlDrean <lucas.henrique.waki@gmail.com>
2022-03-25 16:09:55 -0300

    [fix] - corrected the sql and  the base structure, order and data

--- sql/em_atraso.sql
+++ sql/em_atraso.sql
@@ -4,13 +4,13 @@
 		avg((julianday(delivered_at) - julianday(created_at))) as 'tempo de entrega',
 		avg((julianday(delivered_at) - julianday(delivery_estimate_date))) as 'atraso',
 		avg((julianday(in_transit_to_local_distribution_at) - julianday(created_at))) as 'em transito para distribuidora local',
-		avg((julianday(local_distribution_at) - julianday(created_at))) as ' Chegar na distribuidora Local',
+		avg((julianday(local_distribution_at) - julianday(created_at))) as 'chegar na distribuidora Local',
 		avg((julianday(in_transit_to_deliver_at) -julianday(local_distribution_at))) as  'em armazen',
-		avg((julianday(in_transit_to_deliver_at) - julianday(created_at))) as 'em transito de entrega',
+		avg((julianday(delivered_at) -julianday(in_transit_to_deliver_at))) as  'em transito de entrega',
 
         count(id)
     from logistic
-    WHERE (julianday(delivered_at) - julianday(delivery_estimate_date) >= 1)
+    WHERE ((julianday(delivered_at) - julianday(delivery_estimate_date)) >=1)
 	GROUP by logistic.delivery_addresses_to_state
 	ORDER by avg((julianday(delivered_at) - julianday(created_at)))
 		 desc
